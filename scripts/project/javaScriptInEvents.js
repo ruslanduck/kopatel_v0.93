675143b4ec285163dffafd70cde4697afd305783
ruslanduck <ruslan@duck-agency.com>
2025-06-19 20:12:50 +0200

    Add files via upload

---
+++ scripts/project/javaScriptInEvents.js
@@ -0,0 +1,43 @@
+
+
+const scriptsInEvents = {
+
+	async General_Event33_Act6(runtime, localVars)
+	{
+		const params = new URLSearchParams(window.location.search);
+		let rawName = params.get("name");
+		if (rawName) {
+		    try {
+		        rawName = decodeURIComponent(rawName);
+		    } catch (e) {
+		        // fallback if decodeURIComponent fails
+		        console.warn("Could not decode name, using raw value instead.");
+		    }
+		    runtime.globalVars.name = rawName;
+		}
+		
+	},
+
+	async General_Event34_Act14(runtime, localVars)
+	{
+		const params = new URLSearchParams(window.location.search);
+		let rawName = params.get("name");
+		if (rawName) {
+		    try {
+		        rawName = decodeURIComponent(rawName);
+		    } catch (e) {
+		        // fallback if decodeURIComponent fails
+		        console.warn("Could not decode name, using raw value instead.");
+		    }
+		    runtime.globalVars.name = rawName;
+		}
+		
+	},
+
+	async Digging_sheet_Event608_Act10(runtime, localVars)
+	{
+
+	}
+};
+
+globalThis.C3.JavaScriptInEvents = scriptsInEvents;
